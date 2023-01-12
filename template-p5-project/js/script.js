68555df2c1c1c36684204ec648a57e009f71786b
jaebrn <80350744+jaebrn@users.noreply.github.com>
2023-01-12 15:37:33 -0500

    Exercise 1 (Pong) Setup
    Created canvas, created classes for paddles and ball, added constructors for each and relevant variable. No movement/functionality/interaction

--- template-p5-project/js/script.js
+++ template-p5-project/js/script.js
@@ -7,19 +7,81 @@ Pong in p5js
 */
 
 "use strict";
-
-
-function preload() {
-
-}
-
+//object speeds
+var paddleSpeed = 0;
+var ballSpeed = 10;
+//player scores
+var leftScore = 0;
+var rightScore = 0;
+//object variables (used for instantiation & identification)
+var ball;
+var leftPaddle;
+var rightPaddle;
+//storing key codes for easy recall
+var keycodeW = 87;
+var keycodeS = 83;
+var keycodeUp = 38;
+var keycodeDown = 40;
 
 function setup() {
-    createCanvas(500, 500);
-    background(0, 255, 0);
+    createCanvas(1500, 1000);
+    background(0);
+
+    leftPaddle = new Paddle();
+    leftPaddle.spawn();
+
+    rightPaddle = new Paddle();
+    rightPaddle.posX = width - rightPaddle.sizeX; // setting position to right side of screen
+    rightPaddle.spawn();
+
+    ball = new Ball();
+    ball.spawn();
 }
 
 
 function draw() {
+}
 
+class Paddle {
+    constructor() {
+        //color setup
+        this.fill = 255;
+        //speed
+        this.speed = paddleSpeed;
+        //position
+        this.posX = 0;
+        this.posY = height / 2;
+        //size setup
+        this.sizeX = 25;
+        this.sizeY = 100;
+        //movement bools
+        this.keyUp = false;
+        this.keyDown = true;
+    }
+
+    spawn() {
+        noStroke();
+        fill(this.fill);
+        rect(this.posX, this.posY, this.sizeX, this.sizeY);
+    }
+
+    move() {
+
+    }
+}
+
+class Ball {
+    constructor() {
+        this.fill = 255;
+        this.speed = ballSpeed;
+        this.posX = width / 2;
+        this.posY = height / 2;
+        this.size = 20;
+    }
+
+    spawn() {
+        noStroke();
+        fill(this.fill);
+        circle(this.posX, this.posY, this.size);
+    }
 }
