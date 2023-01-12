ad637685207684e91dd2682cad21ab0419d1fb04
jaebrn <80350744+jaebrn@users.noreply.github.com>
2023-01-12 15:00:27 -0500

    Exercise 1 initial commit
    Adds p5js project template, plus 500x500 green canvas for testing purposes

---
+++ template-p5-project/js/script.js
@@ -0,0 +1,25 @@
+/**
+Exercise 1 - CART 263
+Jenna Brown
+
+Completed week of January 12, 2023
+Pong in p5js
+*/
+
+"use strict";
+
+
+function preload() {
+
+}
+
+
+function setup() {
+    createCanvas(500, 500);
+    background(0, 255, 0);
+}
+
+
+function draw() {
+
+}
