743ca3971dacff2abfd7f7b82e2eb02bcb9d0449
jaebrn <80350744+jaebrn@users.noreply.github.com>
2023-03-08 13:31:50 -0500

    Clouds Added

--- Data-Vis/js/script.js
+++ Data-Vis/js/script.js
@@ -1,6 +1,6 @@
 let weather;
 let time = 0;
-let angle = 235 + 90;
+let angle = 235 + 90; // rotation for sun/moon/stars
 
 //Sky:
 let barCount = 32;
@@ -13,9 +13,17 @@ let starCount = 4000;
 let starArray = [];
 let starOpacity;
 
+//Clouds:
+let cloudArray = [];
+let windSpeed;
+let cloudIndex;
+let cloudCover;
+let cloudCount;
+
 
 function preload() {
     weather = loadJSON('https://api.open-meteo.com/v1/forecast?latitude=-33.95&longitude=151.18&hourly=temperature_2m,rain,showers,snowfall,cloudcover,visibility,windspeed_10m,winddirection_10m&daily=sunrise,sunset&current_weather=true&timezone=Australia%2FSydney');
+
 }
 
 
@@ -31,14 +39,22 @@ function setup() {
         starArray[i] = new Star;
         starArray[i].setup();
     }
+
+    cloudIndex = weather.hourly.cloudcover.length - 1;
+    cloudCover = weather.hourly.cloudcover[cloudIndex];
+    windSpeed = weather.current_weather.windspeed;
+    print(windSpeed);
+    //print(cloudCover);
 }
 
 function draw() {
+
+
     time += 0.1;
     if (time >= 24) {
         time = 0
     }
-    print(time);
+    //print(time);
 
     if (time <= 12) {
         starOpacity = map(time, 0, 12, 360, 0);
@@ -46,11 +62,30 @@ function draw() {
         starOpacity = map(time, 12, 24, 0, 360);
     }
 
+    //Rotating elements:
+    push();
     sky();
     sun();
     for (i = 0; i < starCount; i++) {
         starArray[i].display();
     }
+    pop();
+    //End rotation
+
+    cloudCount = cloudCover * 2;
+    print(cloudCount);
+    if (cloudCount > cloudArray.length) {
+        cloudArray.push(new Cloud);
+        cloudArray[cloudArray.length - 1].spawn();
+    }
+
+    for (i = 0; i < cloudArray.length; i++) {
+        cloudArray[i].move();
+        if (cloudArray[i].x > width) {
+            cloudArray.splice(i, 1);
+        }
+    }
+
 }
 
 function sky() {
@@ -67,6 +102,7 @@ function sun() {
     rotate(time * 15 + 55);
     circle(700, 400, 80);
     circle(700, 400, 100);
+
 }
 
 
@@ -99,7 +135,25 @@ class Cloud {
         this.y;
         this.w;
         this.h;
+        this.angle = 15;
+        this.speed = random(windSpeed / 8, windSpeed / 2);
+    }
 
+    spawn() {
+        this.x = random(-2500, -400);
+        this.y = random(0, 700);
+        this.w = random(10, 400);
+        this.h = random(100, 200);
+        if (this.h >= this.w) {
+            this.w = this.h + 100;
+        }
+    }
+
+    move() {
+        this.x += this.speed;
+        noStroke();
+        fill(0, 0, 360, 100);
+        rect(this.x, this.y, this.w, this.h, this.angle);
     }
 }
 
