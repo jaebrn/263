b50cf2e2392aba61a83ce72f1be65c2f738e6963
jaebrn <80350744+jaebrn@users.noreply.github.com>
2023-03-07 19:42:43 -0500

    Star, Sun, sky

--- Data-Vis/js/script.js
+++ Data-Vis/js/script.js
@@ -1,33 +1,97 @@
-/**
-Title of Project
-Author Name
+let weather;
+let time = 0;
+let angle = 235 + 90;
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
+//Sky:
+let barCount = 32;
+let barHeight;
+let skyHue = 250;
+let skyBright = 50;
 
-"use strict";
+//Stars:
+let starCount = 4000;
+let starArray = [];
+let maxSize = 4;
+let opacity;
 
 
-/**
-Description of preload
-*/
 function preload() {
+    weather = loadJSON('https://api.open-meteo.com/v1/forecast?latitude=-33.95&longitude=151.18&hourly=temperature_2m,rain,showers,snowfall,cloudcover,visibility,windspeed_10m,winddirection_10m&daily=sunrise,sunset&current_weather=true&timezone=Australia%2FSydney');
+
 
 }
 
 
-/**
-Description of setup
-*/
 function setup() {
+    createCanvas(1920, 1080);
+    background(0);
+    colorMode(HSB, 360);
+    angleMode(DEGREES);
 
+    barHeight = height / barCount;
+
+    for (i = 0; i < starCount; i++) {
+        starArray[i] = new Star;
+        starArray[i].setup();
+    }
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+    time += 0.01;
+
+    sky();
+    sun();
+    for (i = 0; i < starCount; i++) {
+        starArray[i].display();
+    }
+}
+
+function sky() {
+    for (i = 0; i < barCount; i++) {
+        noStroke();
+        fill(skyHue - i, 200, i * 7 + skyBright);
+        rect(0, i * barHeight, width, barHeight);
+    }
+}
+
+function sun() {
+    fill(360, 0, 360, 250);
+    translate(width / 2, height / 1.1);
+    rotate(time * 15 + 55);
+    circle(700, 400, 80);
+    circle(700, 400, 100);
+}
+
+function stars() {
+
 
 }
+
+class Star {
+    constructor() {
+        this.x;
+        this.y;
+        this.maxSize = 4;
+        this.size;
+        this.visibility;
+        this.opacity = 200;
+    }
+
+    setup() {
+        this.size = random(1, maxSize);
+        this.x = random(-width, width * 2);
+        this.y = random(-height, height * 2);
+    }
+
+    display() {
+        this.opacity += random(-50, 50);
+        fill(200, 45, 360, this.opacity);
+        circle(this.x, this.y, this.size);
+    }
+}
+
+//Reference
+    // cloudIndex = weather.hourly.cloudcover.length - 1;
+    // cloudCover = weather.hourly.cloudcover[cloudIndex];
+    // text("Current wind speed:" + weather.current_weather.windspeed, 250, 250);
+    // text("Current wind direction:" + weather.current_weather.winddirection, 250, 285);
